dafb8dccdf2000187dd63958d6bb57d40ad1d167
birbjam <41635757+birbjam@users.noreply.github.com>
2019-06-11 21:03:00 -0700

    migrated table to knex, commented out the schema dummy data

--- migrations/20190610210013_events.js
+++ migrations/20190610210013_events.js
@@ -1,8 +1,19 @@
 
 exports.up = function (knex, Promise) {
-
+  return knex.schema
+    .createTable('classes', function (table) {
+      table.increments('id') // Adds an auto incrementing column and uses it as the primary key
+      table.string('teacher', 255).notNullable()
+      table.string('eventname', 255).notNullable()
+      table.string('category', 255).notNullable()
+      table.string('eventlocation', 255).notNullable()
+      table.string('eventdate', 255).notNullable()
+      table.string('eventtime', 255).notNullable()
+      table.string('eventdescription', 255).notNullable();
+    })
 }
 
 exports.down = function (knex, Promise) {
-
+  return knex.schema
+    .dropTable('classes')
 }
