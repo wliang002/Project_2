12efe84a28cf82380bf1092de3c38a81a627b747
Swiftlet <hettybootcamp@gmail.com>
2019-06-14 15:55:13 -0700

    modified classes.js and apiRoutes.js to get the buttons on index.html that are created in index.js to only pull records for the selected categories

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,21 +9,28 @@ const Class = require('../models/classes')
  */
 
 module.exports = function (app) {
-  // Get all classes
-  app.get('/learn', function (req, res) {
+  // this gets all classes by returning the entire dataset -- used for page category buttons via index.js
+  app.get('/allData', function (req, res) {
     Class.findAll()
       .then(function (dbClasses) {
         res.json(dbClasses)
       })
   })
 
+  // this only returns classes from the chosen category -- for "learn" pages
   app.get('/learn/:category', function (req, res) {
-    var chosen = req.params.category
-
-    console.log(`category selected: ${chosen}`)
+    var chosenCategory = req.params.category
+    // this logs in the terminal
+    console.log(`category selected: ${chosenCategory}`)
+    Class.find({
+      category: chosenCategory }
+    ).then(results => {
+      console.log(`Class.find(): ${results.length ? results : 'No records found'}`)
+      res.json(results)
+    })
   })
 
-  // Create a new class
+  // this creates a new class in the database -- for teach
   app.post('/teach', function (req, res) {
     Class.create(req.body)
       .then(function (dbClass) {
@@ -31,11 +38,24 @@ module.exports = function (app) {
       })
   })
 
-  // Delete a class by id
-  app.delete('/teach/:id', function (req, res) {
-    Class.destroy(req.params)
-      .then(function (dbClass) {
-        res.json(dbClass)
-      })
+  // this only returns classes from a chosen teacher -- for "teach" pages
+  app.get('/teach/:teacher', function (req, res) {
+    var chosenTeacher = req.params.teacher
+    console.log(`category selected: ${chosenTeacher}`)
+    Class.find({
+      category: chosenTeacher }
+    ).then(results => {
+      console.log(`Class.find(): ${results.length ? results : 'No records found'}`)
+      res.json(results)
+    })
   })
+
+  // // !!!! this one needs to be reworked based on how teach page wants to delete classes
+  // // Delete a class by id
+  // app.delete('/teach/:id', function (req, res) {
+  //   Class.destroy(req.params)
+  //     .then(function (dbClass) {
+  //       res.json(dbClass)
+  //     })
+  // })
 }
