8f96b045af9f82ef33c2f50cb4ed813acd768c84
birbjam <41635757+birbjam@users.noreply.github.com>
2019-06-15 08:50:22 -0700

    Cleaned up some unused code, did some formatting, added a footer

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,15 @@
 // Dependecies
-// const tableData = require('../models/classes')
 
 const path = require('path')
 const Classes = require('../models/classes')
 /**
  * htmlRoutes: This routes file renders views e.g. handlebars pages
  * It differs from the apiRoutes.js file in that it responds to the client/view requests with a
- * handlebars page where the apiRoutes.js responds with data onlu
+ * handlebars page where the apiRoutes.js responds with data only
  *
  */
 
-
-// code to use handlebars
-// Dependencies
+// Code to use handlebars
 
 module.exports = function (app) {
 // Load index page
@@ -39,8 +36,8 @@ module.exports = function (app) {
         categories.push(category)
       }
     })
-    
-   cb(categories)
+
+    cb(categories)
   }
 
   app.get('/learn/:category', function (req, res) {
@@ -50,13 +47,13 @@ module.exports = function (app) {
       category: chosenCategory
     })
       .then(function (dbClasses) {
-            filterCategories(dbClasses, function (results) {
-              console.log('category chosen   '+results)
-              res.render('learn', {
-                categories: results,
-                classes: dbClasses
-              })
-            })
+        filterCategories(dbClasses, function (results) {
+          console.log('category chosen   ' + results)
+          res.render('learn', {
+            categories: results,
+            classes: dbClasses
+          })
+        })
       })
   })
 
