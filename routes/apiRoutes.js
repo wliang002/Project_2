392ee3a005dd012051f6775db08706774b8df18e
wliang002 <wliang002@berkeley.edu>
2019-06-14 17:19:02 -0700

    teach.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,24 +9,50 @@ const Class = require('../models/classes')
  */
 
 module.exports = function (app) {
-  // Get all classes
-  app.get('/api/events', function (req, res) {
+  // this gets all classes by returning the entire dataset -- used for page category buttons via index.js
+  app.get('/allData', function (req, res) {
     Class.findAll()
       .then(function (dbClasses) {
         res.json(dbClasses)
       })
   })
 
-  // Create a new class
-  app.post('/api/events', function (req, res) {
+  // this only returns classes from the chosen category -- for "learn" pages
+  app.get('/learn/:category', function (req, res) {
+    var chosenCategory = req.params.category
+    // this logs in the terminal
+    console.log(`category selected: ${chosenCategory}`)
+    Class.find({
+      category: chosenCategory }
+    ).then(results => {
+      console.log(`Class.find(): ${results.length ? results : 'No records found'}`)
+      res.json(results)
+    })
+  })
+
+  // this creates a new class in the database -- for teach
+  app.post('/teach', function (req, res) {
     Class.create(req.body)
       .then(function (dbClass) {
         res.json(dbClass)
       })
   })
 
+  // this only returns classes from a chosen teacher -- for "teach" pages
+  app.get('/teach/:teacher', function (req, res) {
+    var chosenTeacher = req.params.teacher
+    console.log(`category selected: ${chosenTeacher}`)
+    Class.find({
+      category: chosenTeacher }
+    ).then(results => {
+      console.log(`Class.find(): ${results.length ? results : 'No records found'}`)
+      res.json(results)
+    })
+  })
+
+  // // !!!! this one needs to be reworked based on how teach page wants to delete classes
   // Delete a class by id
-  app.delete('/api/events/:id', function (req, res) {
+  app.delete('/teach/:id', function (req, res) {
     Class.destroy(req.params)
       .then(function (dbClass) {
         res.json(dbClass)
